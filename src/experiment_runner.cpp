a9ec68c26a6cfd11e585563bab97974c854242da
Javier Velez <velezj@alum.mit.edu>
2014-04-02 09:24:32 -0400

    removing warning compile flag. starting to write a permutation test runner but it is not finished

--- src/experiment_runner.cpp
+++ src/experiment_runner.cpp
@@ -83,6 +83,65 @@ namespace point_process_experiment_core {
   
 
   //====================================================================
+
+  void
+  run_permutation_entropy_trace
+  ( const std::string& world,
+    const std::string& model,
+    const std::string& planner_id,
+    const std::string& experiment_id )
+  {
+
+    // // push the experiment id as a context
+    // p2l::common::push_context( p2l::common::context_t( experiment_id ) );
+    
+    // // get the wanted world points and window
+    // std::vector< nd_point_t > ground_truth = groundtruth_for_world( world );
+    // nd_aabox_t world_window = window_for_world( world );
+    
+    // // build up the point process model
+    // boost::shared_ptr< mcmc_point_process_t > planner_process 
+    //   = get_model_by_id( model, world_window, ground_truth );
+    
+    // // build up the planner
+    // boost::shared_ptr<grid_planner_t> planner 
+    //   = get_planner_by_id( planner_id, planner_process  );
+
+    // // create the meta and trace files
+    // std::ofstream out_meta( p2l::common::context_filename( "planner.meta" ) );
+    // std::ofstream out_trace( p2l::common::context_filename( "planner.trace" ) );
+    // std::ofstream out_verbose_trace( p2l::common::context_filename( "planner.verbose-trace" ) );
+    // std::cout << "context filename are in: " << p2l::common::context_filename( "<filename>") << std::endl;
+    
+    
+    // // run the planner to get first permutation of observations
+    // bool add_empty_regions = true;
+    // nd_aabox_t initial_window;
+    // double fraction_truth_to_find = 1.0;
+    // std::vector<marked_grid_cell_t> trace =
+    //   simulate_run_until_all_points_found( planner,
+    // 					   add_empty_regions,
+    // 					   initial_window,
+    // 					   fraction_truth_to_find,
+    // 					   ground_truth,
+    // 					   out_meta,
+    // 					   out_trace,
+    // 					   std::cout,
+    // 					   out_verbose_trace);
+
+
+    // // Ok, now we will run through the permutations of the 
+    // // observations
+    // std::vector<double> entropies
+    //   = calculate_entropy_from_fixed_observation_sequence
+    //   ( world,
+    // 	model,
+    // 	planner_id,
+    // 	random_permutation( trace ) );
+    
+  }
+
+
   //====================================================================
   //====================================================================
   //====================================================================
--- src/experiment_runner.hpp
+++ src/experiment_runner.hpp
@@ -21,6 +21,21 @@ namespace point_process_experiment_core {
     const double& fraction_truth_to_find,
     const std::string& experiment_id );
 
+  //=======================================================================
+
+  // Description:
+  // Runs the given planner on a model and world.
+  // We then replay different orderings of the found
+  // locations, and compute the entropy of the system after each
+  // observation
+  void
+  run_permutation_entropy_trace
+  ( const std::string& world,
+    const std::string& model,
+    const std::string& planner,
+    const std::string& experiment_id );
+
+
 
 }
 
